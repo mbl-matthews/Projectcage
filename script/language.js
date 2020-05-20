85a6ccf552c0c9da7e31b3f4d1bd7bb5c3b671bb
Lukas H <lukas.h2@hotmail.de>
2020-05-20 11:21:01 +0200

    refactor language

--- script/language.js
+++ script/language.js
@@ -24,27 +24,44 @@ function getLanguage() {
     return window.navigator.language;
 }
 
-function translate(dict, lang, word) {
-    console.log(dict)
+function translate(word) {
+    let lang = getLanguage()
+  /*  console.log(dict)
     console.log(lang)
-    console.log(word)
+    console.log(word)*/
+    word = word.replace(/[\{\}]/g, '')
     return dict[lang][word];
 }
 
 
-function applyTemplate(tmpl, lang) {
-    //match string with/between {{}}
- /*   const regex = /\{\{(.)+\}\}/g
-    return tmpl.replace(regex, function (word) {
-        return translate(dict, lang, word.replace(/[\{\}]/g, ''));
-    });*/
-    return translate(dict, lang, tmpl.replace(/[\{\}]/g, ''));
 
+function translatePage() {
+    let words = document.getElementsByClassName('translate');
+    let translation = document.getElementsByClassName('translation');
+    console.log(words.length)
+
+    //  console.log(translation)
+    for (let i = 0; i < words.length; i++) {
+        let test = words[i].textContent;
+        let html = translate(test);
+        translation[i].textContent = html;
+    }
 }
 
 
+/*
+function applyTemplate(tmpl) {
+    //match string with/between {{}}
+ //  const regex = /\{\{(.)+\}\}/g
+ //   return tmpl.replace(regex, function (word) {
+//        return translate(dict, lang, word.replace(/[\{\}]/g, ''));
+ //   });
+    return translate(tmpl.replace(/[\{\}]/g, ''));
+
+}*/
+
+/*
 function translatePage() {
-    let lang = getLanguage();
     let words = document.getElementsByClassName('translate');
     let translation  = document.getElementsByClassName('translation');
     console.log(words.length)
@@ -53,12 +70,12 @@ function translatePage() {
     for(let i=0;i<words.length;i++){
         let test=words[i].textContent;
       //  console.log(test)
-        let html = applyTemplate(test, lang);
+        let html = applyTemplate(test);
       //  console.log("aaa:    ",translation[i])
         translation[i].textContent=html;
     }
     console.log(words.length)
-}
+}*/
 
 
 
